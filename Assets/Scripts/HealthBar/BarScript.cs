1290fb6cc9bfe97d57b4568438434269169a87e2
xzhou29 <xin9186@gmail.com>
2016-12-07 22:58:33 -0600

    Add files via upload

---
+++ Assets/Scripts/HealthBar/BarScript.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image content;
+
+   // [SerializeField]
+   // private Text valueText;
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    private float fillAmount;
+
+    [SerializeField]
+    private bool lerpColors;
+
+    [SerializeField]
+    private bool lerpBar;
+
+    [SerializeField]
+    private Color fullColor;
+
+    [SerializeField]
+    private Color lowColor;
+
+    public float MaxValue { get; set; }
+
+    public float Value
+    {
+        set
+        {
+            fillAmount = Map(value, 0, MaxValue, 0, 1);
+        }
+    }
+
+    void Start()
+    {
+        if (lerpColors) 
+        {
+            content.color = fullColor;
+        }
+    }
+
+    void Update ()
+    {
+        HandleBar();
+
+    }
+
+    private void HandleBar()
+    {
+        if (fillAmount != content.fillAmount) 
+        {
+            if (lerpBar)
+            {
+                content.fillAmount = Mathf.Lerp(content.fillAmount, fillAmount, Time.deltaTime * lerpSpeed);
+            }
+            else
+            {
+                content.fillAmount = fillAmount;
+            }
+
+            if (lerpColors)
+            {   
+                content.color = Color.Lerp(lowColor, fullColor, fillAmount);
+            }
+           
+        }
+    }
+
+    /// <param name="value">The value to evaluate</param>
+    /// <param name="inMin">The minimum value of the evaluated variable</param>
+    /// <param name="inMax">The maximum value of the evaluated variable</param>
+    /// <param name="outMin">The minum number we want to map to</param>
+    /// <param name="outMax">The maximum number we want to map to</param>
+    /// <returns></returns>
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
---
+++ Assets/Scripts/HealthBar/Stat.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[Serializable]
+class Stat
+{
+
+    [SerializeField]
+    private BarScript bar;
+
+    private float maxVal;
+
+    private float currentVal;
+
+    public float CurrentValue
+    {
+        get
+        {
+            return currentVal;
+        }
+        set
+        {
+            this.currentVal = Mathf.Clamp(value, 0, MaxVal);
+            //Updates the bar
+            Bar.Value = currentVal;
+        }
+    }
+
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+        set
+        {
+            //Updates the bar's max value
+            Bar.MaxValue = value;
+            this.maxVal = value;
+        }
+    }
+
+    public BarScript Bar
+    {
+        get
+        {
+            return bar;
+        }
+    }
+
+    public void Initialize()
+    {
+        this.MaxVal = maxVal;
+        this.CurrentValue = currentVal;
+    }
+}
+
